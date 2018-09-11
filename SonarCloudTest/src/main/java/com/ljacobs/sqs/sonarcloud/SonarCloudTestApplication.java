3126eae9d803f49dca581b735929d67ef7ad66a7
Jacobs, Laris <laris.jacobs@mt-ag.com>
2018-09-11 11:40:53 +0200

    create Project structure

---
+++ SonarCloudTest/src/main/java/com/ljacobs/sqs/sonarcloud/SonarCloudTestApplication.java
@@ -0,0 +1,12 @@
+package com.ljacobs.sqs.sonarcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarCloudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarCloudTestApplication.class, args);
+	}
+}
